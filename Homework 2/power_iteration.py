9b655f5166cfbc66c0e1a98e1b60a0ed70909d03
Maksim Aksenovsky <100002347+Solitum26@users.noreply.github.com>
2023-11-18 00:19:57 +0300

    fix power_iteration

--- Homework 2/power_iteration.py
+++ Homework 2/power_iteration.py
@@ -1,14 +1,21 @@
 import numpy as np
 
+
 def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
     """
     data: np.ndarray – symmetric diagonalizable real-valued matrix
     num_steps: int – number of power method steps
-    
+
     Returns:
     eigenvalue: float – dominant eigenvalue estimation after `num_steps` steps
     eigenvector: np.ndarray – corresponding eigenvector estimation
     """
     ### YOUR CODE HERE
+    vector = np.random.rand(data.shape[1])
+    
+    for _ in range(num_steps):
+        vector = data.dot(vector)
+        vector = vector / np.linalg.norm(vector)
 
-    return 
+    eigenvalue = vector.dot(data.dot(vector))
+    return eigenvalue.item(), vector
